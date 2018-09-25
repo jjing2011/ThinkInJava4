fe72c63f20a14087dd6a75dfdd83913cae1702a4
jjing2011 <jjing2011@gmail.com>
2018-09-25 17:44:16 +0800

    Inner class

---
+++ src/com/mycompany/ThinkInJava4/interfaces/Games.java
@@ -0,0 +1,49 @@
+package com.mycompany.ThinkInJava4.interfaces;
+
+import com.mycompany.ThinkInJava4.Print;
+
+interface Game {
+    boolean move();
+}
+
+interface GameFactory {
+    Game getGame();
+}
+
+class Checkers implements Game {
+    private int moves = 0;
+    private static final int MOVES = 3;
+    public boolean move() {
+        Print.print("Checkers move" + moves);
+        return ++moves != MOVES;
+    }
+}
+
+class CheckersFactory implements GameFactory {
+    public Game getGame() { return new Checkers(); }
+}
+
+class Chess implements Game {
+    private int moves = 0;
+    private static final int MOVES = 4;
+    public boolean move() {
+        Print.print("Chess move" + moves);
+        return ++moves != MOVES;
+    }
+}
+
+class ChessFactory implements GameFactory {
+    public Game getGame() { return new Chess(); }
+}
+
+public class Games {
+    public static void playGame(GameFactory factory) {
+        Game s = factory.getGame();
+        while(s.move()) {}
+    }
+
+    public static void main(String[] args) {
+        playGame(new CheckersFactory());
+        playGame(new ChessFactory());
+    }
+}
